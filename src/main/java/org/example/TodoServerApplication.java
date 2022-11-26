476fc15c12d53c3a56d88da45b1ab84afdbaddd4
sy2298@naver.com <39763694+sy2298@users.noreply.github.com>
2022-11-26 18:17:14 +0900

    add repository and service

--- src/main/java/org/example/TodoServerApplication.java
+++ src/main/java/org/example/TodoServerApplication.java
@@ -1,11 +1,13 @@
 package org.example;
 
 import org.example.repository.TodoRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class TodoServerApplication {
     public static void main(String[] args) {
-        System.out.println("hello");
-        TodoRepository repository;
-        
+        SpringApplication.run(TodoServerApplication.class, args);
+
     }
 }
